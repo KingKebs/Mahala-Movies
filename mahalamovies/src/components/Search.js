608f20a8d778e2ec8c52109bccbfd801e3492a50
KingKebs <kebogile1201@icloud.com>
2020-05-22 01:46:32 +0200

    extracting display card to own compnent
    moving display card to own component

--- mahalamovies/src/components/Search.js
+++ mahalamovies/src/components/Search.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Moviecard from "./Moviecard";
 
 function Search() {
   // Adding states
@@ -15,6 +16,7 @@ function Search() {
     try {
       const res = await fetch(url);
       const data = await res.json();
+      //   setMovies sets results object "data" from the MOviedb fetch
       setMovies(data.results);
       console(data.results);
     } catch (err) {
@@ -45,23 +47,7 @@ function Search() {
         {movies
           .filter((movie) => movie.poster_path)
           .map((movie) => (
-            <div className="card" key={movie.id}>
-              <img
-                className="card--image"
-                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                alt={movie.title + " poster"}
-              />
-              <div className="card--content">
-                <h3 className="card--title">{movie.title}</h3>
-                <p>
-                  <small>RELEASE DATE: {movie.release_date}</small>
-                </p>
-                <p>
-                  <small>RATING: {movie.vote_average}</small>
-                </p>
-                <p className="card--desc">{movie.overview}</p>
-              </div>
-            </div>
+            <Moviecard movie={movie} key={movie.id} />
           ))}
       </div>
     </>
