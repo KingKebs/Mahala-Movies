bea5ff920e8b10f093541ace4f44dc3268147ac7
KingKebs <kebogile1201@icloud.com>
2020-05-21 14:23:50 +0200

    API response config
    async functionality to grab movie data as json object
    setting movie title and grabbing movie images

--- mahalamovies/src/components/Search.js
+++ mahalamovies/src/components/Search.js
@@ -3,40 +3,55 @@ import React, { useState } from "react";
 function Search() {
   // Adding states
   const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (e) => {
     e.preventDefault();
 
-    const api = "5dcf7f28a88be0edc01bbbde06f024ab";
+    const apikey = "5dcf7f28a88be0edc01bbbde06f024ab";
 
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${api}&language=en-US&query=${query}&page=1&include_adult=false`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
       const res = await fetch(url);
       const data = await res.json();
-      console.log(data);
+      setMovies(data.results);
     } catch (err) {
       console.error(err);
     }
   };
 
   return (
-    <form className="form" onSubmit={searchMovies}>
-      <label className="label" htmlFor="query">
-        Movie Name
-      </label>
-      <input
-        className="input"
-        type="text"
-        name="query"
-        placeholder="Enter a movie"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <button className="button" type="submit">
-        Search
-      </button>
-    </form>
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="Enter a movie"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+      {/* display the movie information card  */}
+      <div class="card-list">
+        {movies.map((movie) => (
+          <div className="card" key={movie.id}>
+            <img
+              className="card--image"
+              src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+              alt={movie.title + " poster"}
+            />
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
