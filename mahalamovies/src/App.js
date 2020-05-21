8b9900724cc96ad01b53e8b06d02bfb6362e7049
KingKebs <kebogile1201@icloud.com>
2020-05-21 02:51:49 +0200

    remove default react settings
    init

---
+++ mahalamovies/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Search from "./components/Search";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1 className="title"> Pule's Flix </h1>
+      <Search />
+    </div>
+  );
+}
